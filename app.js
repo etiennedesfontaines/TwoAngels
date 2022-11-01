b4be72f5e1a4514fb9b4de2c0f54584ab04a3468
Etienne Desfontaines <erdesfontaines@gmail.com>
2022-11-01 13:59:06 +0200

    content

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ carouselButtons.forEach((button) => {
 		const activeSlide = slides.querySelector("[data-active]");
 		const indicators = button.closest(".image-carousel").querySelector(".progress-indicators"); //prettier-ignore
 		const activeIndicator = indicators.querySelector("[data-active]");
+
 		let newIndex = [...slides.children].indexOf(activeSlide) + buttonValue;
+
 		if (newIndex < 0) {
 			newIndex = slides.children.length - 1;
 		}
@@ -25,18 +27,28 @@ carouselButtons.forEach((button) => {
 	});
 });
 
-progressIndicators.forEach((indicator) => {
-	indicator.addEventListener("click", () => {
+progressIndicators.forEach((indicator, index) => {
+	indicator.addEventListener("click", (e) => {
 		const indicators = indicator.closest(".image-carousel").querySelector(".progress-indicators"); //prettier-ignore
 		const activeIndicator = indicators.querySelector("[data-active]");
 		const slides = indicators.closest(".image-carousel").querySelector(".slides"); //prettier-ignore
 		const activeSlide = slides.querySelector("[data-active]");
+		if (activeIndicator.innerHTML !== e.target.innerHTML) {
+			indicators.children[index].dataset.active = "true";
+			slides.children[index].dataset.active = "true";
 
-		indicators.children[indicator.innerHTML - 1].dataset.active = "true";
-		slides.children[indicator.innerHTML - 1].dataset.active = "true";
-		delete activeIndicator.dataset.active;
-		delete activeSlide.dataset.active;
+			delete activeIndicator.dataset.active;
+			delete activeSlide.dataset.active;
+		}
 	});
 });
 
 //IMAGE CAROUSEL CLOSE
+
+//CONTACT OPEN
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log("submitted");
+});
+//CONTACT CLOSE
