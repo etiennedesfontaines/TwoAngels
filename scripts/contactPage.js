8266885a37eb7b092bd864ea00e7e903c092d2f8
Etienne Desfontaines <erdesfontaines@gmail.com>
2023-03-29 17:46:17 +0200

    refactor-05

--- scripts/contactPage.js
+++ scripts/contactPage.js
@@ -1,16 +1,40 @@
 const form = document.querySelector("form");
 const formWrapper = document.querySelector(".contact-page__form-wrapper");
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-});
+// form.addEventListener("submit", (e) => {
+// 	e.preventDefault();
+// });
 
-onsubmit = (e) => {
-	form.classList.add("contact-form--submitted");
+// onsubmit = (e) => {
+// 	form.classList.add("contact-form--submitted");
 
-	const submitted = document.createElement("h3");
-	submitted.innerHTML = "Submission Successful";
-	submitted.classList.add("contact-page__subheading");
-	formWrapper.classList.add("contact-page__form-wrapper--submitted");
-	formWrapper.appendChild(submitted);
+// 	const submitted = document.createElement("h3");
+// 	submitted.innerHTML = "Submission Successful";
+// 	submitted.classList.add("contact-page__subheading");
+// 	formWrapper.classList.add("contact-page__form-wrapper--submitted");
+// 	formWrapper.appendChild(submitted);
+// };
+
+const handleSubmit = (event) => {
+	event.preventDefault();
+
+	const myForm = event.target;
+	const formData = new FormData(myForm);
+
+	fetch("/", {
+		method: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: new URLSearchParams(formData).toString(),
+	})
+		.then(() => {
+			form.classList.add("contact-form--submitted");
+			const submitted = document.createElement("h3");
+			submitted.innerHTML = "Submission Successful";
+			submitted.classList.add("contact-page__subheading");
+			formWrapper.classList.add("contact-page__form-wrapper--submitted");
+			formWrapper.appendChild(submitted);
+		})
+		.catch((error) => alert(error));
 };
+
+form.addEventListener("submit", handleSubmit);
