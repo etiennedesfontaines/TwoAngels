ee5563b94d553f4ff9924821950e5cb1cce7b65e
Etienne Desfontaines <erdesfontaines@gmail.com>
2023-03-31 17:41:26 +0200

    refactor-13

--- scripts/lazyImage.js
+++ scripts/lazyImage.js
@@ -3,18 +3,27 @@
 // Let’s say we specify the image URL in the data-src attribute of the image tag.
 // Now that src is empty, the browser doesn’t trigger the image load
 
-// const img = document.querySelector(".carousel-image-1");
+//firstCarouselImage will likely need to be a little more of a complex selector for the suites
+//They have multiple carousels with a first image in which I want to apply the below logic.
+const firstCarouselImage = document.querySelector(".image-carousel__image--first"); //prettier-ignore
+const carouselImages = firstCarouselImage
+	.closest(".image-carousel")
+	.querySelectorAll(".image-carousel__image");
 
-// otherImages.forEach((img) =>{
-// img.src = img.data-src;
-// })
+//I am uncertain I want to load all the rest of the images at once
+// May be more performant to load one at a time and then retrigger load on each completion?
+const loadImages = () => {
+	carouselImages.forEach((img) => {
+		if (img !== firstCarouselImage) {
+			img.src = img.dataset.src;
+		}
+	});
+};
 
-// function loaded() {
-// 	console.log("The Image Has Loaded succesfully");
-// }
+firstCarouselImage.addEventListener("load", loadImages);
 
 // if (img.complete) {
-// 	loaded();
+// 	loadImages();
 // } else {
 // 	img.addEventListener("load", loaded);
 // 	img.addEventListener("error", function () {
