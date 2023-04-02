7fec2f90ced7cf129823a24c2a2f750ea27d5fd0
Etienne Desfontaines <erdesfontaines@gmail.com>
2023-04-02 15:51:12 +0200

    refactor-14

--- scripts/lazyImage.js
+++ scripts/lazyImage.js
@@ -1,32 +1,27 @@
-//the browser uses the src attribute of the tag to trigger the image load
-//to lazyload such images, put the image URL in an attribute other than src.
-// Let’s say we specify the image URL in the data-src attribute of the image tag.
-// Now that src is empty, the browser doesn’t trigger the image load
+const firstCarouselImages = document.querySelectorAll(".image-carousel__image--first"); //prettier-ignore
 
-//firstCarouselImage will likely need to be a little more of a complex selector for the suites
-//They have multiple carousels with a first image in which I want to apply the below logic.
-const firstCarouselImage = document.querySelector(".image-carousel__image--first"); //prettier-ignore
-const carouselImages = firstCarouselImage
-	.closest(".image-carousel")
-	.querySelectorAll(".image-carousel__image");
+const loadRemainingCarouselImages = (firstImage) => {
+	const carouselImages = firstImage
+		.closest(".image-carousel")
+		.querySelectorAll(".image-carousel__image");
 
-//I am uncertain I want to load all the rest of the images at once
-// May be more performant to load one at a time and then retrigger load on each completion?
-const loadImages = () => {
 	carouselImages.forEach((img) => {
-		if (img !== firstCarouselImage) {
+		if (img !== firstImage) {
 			img.src = img.dataset.src;
+			console.log("another image loaded");
 		}
 	});
 };
 
-firstCarouselImage.addEventListener("load", loadImages);
+firstCarouselImages.forEach((img) => {
+	img.addEventListener("load", () => {
+		console.log("first image loaded");
+		loadRemainingCarouselImages(img);
+	});
+	if (firstCarouselImages[0] === img || firstCarouselImages[1] === img) {
+		img.src = img.dataset.src;
+	}
+});
 
-// if (img.complete) {
-// 	loadImages();
-// } else {
-// 	img.addEventListener("load", loaded);
-// 	img.addEventListener("error", function () {
-// 		alert("error");
-// 	});
-// }
+//recursion - a function calling itself.
+//typescript
